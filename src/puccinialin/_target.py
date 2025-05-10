fff536a7ebfc3d05e373d34c1bbbfa9a1ec8b0a5
konstin <konstin@mailbox.org>
2025-05-11 00:20:01 +0200

    Try fixing mac

--- src/puccinialin/_target.py
+++ src/puccinialin/_target.py
@@ -55,29 +55,29 @@ def get_triple(file: typing.IO) -> str:
     if soabi:
         print(f"Python reports SOABI: {soabi}", file=file)
         if soabi.count("-") == 1:
-            _python, platform = soabi.split("-", maxsplit=2)
+            _python, sysconfig_platform = soabi.split("-", maxsplit=2)
         else:
-            _python_interpreter, _python_version, platform = soabi.split(
+            _python_interpreter, _python_version, sysconfig_platform = soabi.split(
                 "-", maxsplit=2
             )
     else:
         # Older windows doesn't have SOABI
-        platform = sysconfig.get_platform()
-        print(f"Python reports platform: {platform}", file=file)
+        sysconfig_platform = sysconfig.get_platform()
+        print(f"Python reports platform: {sysconfig_platform}", file=file)
 
     # Linux
-    if "-linux-" in platform:
+    if "-linux-" in sysconfig_platform:
         try:
-            arch, linux, libc = platform.split("-")
+            arch, linux, libc = sysconfig_platform.split("-")
         except ValueError:
-            print(f"Unrecognized linux platform {platform}", file=file)
+            print(f"Unrecognized linux platform {sysconfig_platform}", file=file)
             sys.exit(1)
         if not linux == "linux":
-            print(f"Unrecognized linux platform {platform}", file=file)
+            print(f"Unrecognized linux platform {sysconfig_platform}", file=file)
             sys.exit(1)
         target = f"{arch}-unknown-{linux}-{libc}"
     # macOS (both Intel and arm)
-    elif platform == "darwin":
+    elif sysconfig_platform == "darwin":
         # Check machine architecture to determine the correct target
         machine = platform.machine()
         if machine == "arm64":
@@ -87,13 +87,13 @@ def get_triple(file: typing.IO) -> str:
         else:
             raise ValueError(f"Unknown macOS machine: {machine}")
     # Windows x86_64
-    elif platform in ["win-amd64", "win_amd64"]:
+    elif sysconfig_platform in ["win-amd64", "win_amd64"]:
         target = "x86_64-pc-windows-msvc"
     # Windows x86
-    elif platform == "win32":
+    elif sysconfig_platform == "win32":
         target = "i686-pc-windows-msvc"
     else:
-        print(f"Unsupported platform: {platform}", file=file)
+        print(f"Unsupported platform: {sysconfig_platform}", file=file)
         sys.exit(1)
     print(f"Computed rustc target triple: {target}", file=file)
 
